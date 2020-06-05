63e27520c756730dd2d17a07d102000189074cbe
blipp <bclipp770@gmail.com>
2020-06-05 06:50:34 -0400

    init commit

---
+++ app/main.py
@@ -0,0 +1,7 @@
+# feature
+# fill in missing data in pg tables customers and stores
+    #  blockID or block fips id, state_fips, state code and block population
+
+    # using backoff, call census api
+    # interact with db (crud)
+    # iterate on customer and stores looking for rows with missing data then look up
