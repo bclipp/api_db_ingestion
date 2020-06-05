7bf31f4452f3feb95596336d77a79e283e852859
blipp <bclipp770@gmail.com>
2020-06-05 06:53:53 -0400

    starting census test

---
+++ test/test_census_api.py
@@ -0,0 +1,16 @@
+
+"""
+"""
+
+import pytest
+
+import app.census_api
+
+TESTDATA = [("a", 5, "aaaaa")]
+
+
+@pytest.mark.parametrize("character,count,wanted", TESTDATA)
+def test_repeat(character: str, count: int, wanted: str):
+    repeated: str = repeat.repeat(character, count)
+    print(f"repeated {repeated} wanted {wanted}")
+    assert repeated == wanted
