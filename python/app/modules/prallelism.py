5cf81fe9f1ab6d696135a7f656dd05fde45e139b
blipp <bclipp770@gmail.com>
2020-06-11 07:03:03 -0400

    adding con and parallelism

--- python/app/modules/prallelism.py
+++ python/app/modules/prallelism.py
@@ -1,5 +1,24 @@
-def concurrent_me():
-    print("im concurrent")
+"""
+This module is used to hold concurrency code
+"""
+
+import multiprocessing as mp
+from typing import Callable
+
+
+def concurrent_me(size: int, func: Callable, data: list):
+    """
+    This is a simple function to make processing a list of dict concurrent
+    :param size:
+    :param func:
+    :param data:
+    :return:
+    """
+    pool: mp.Pool = mp.Pool(size)
+    updated_data: list = pool.map(func, data)
+    pool.close()
+    pool.join()
+    return updated_data
 
 
 def multithread_me():
