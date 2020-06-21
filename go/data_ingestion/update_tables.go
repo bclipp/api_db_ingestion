37583573335f2daeb373a78b93f44fa9092b8118
blipp <bclipp770@gmail.com>
2020-06-21 07:29:49 -0400

    workng up updating table func

--- go/data_ingestion/update_tables.go
+++ go/data_ingestion/update_tables.go
@@ -1,12 +1,21 @@
 package data_ingestion
 
-func update_tabls(concurrent bool ){
+func update_tabls(concurrent bool, tables []string, database *Database ){
+
+
 	if concurrent == false {
-		//loop over each tables
-		//read table
-		// interate of table
-		// lookup each row
-		// updating table
+		for _, table := range tables {
+			database.Connect()
+			defer database.Db.Close()
+			database.ReadTable(table)
+			for _, row := range database.table {
+				println(row)
+
+				// lookup each row
+				// updating table in db
+			}
+		}
+
 	}
 
 }
